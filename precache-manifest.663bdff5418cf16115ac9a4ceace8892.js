5784726769d233cc734f4b40b5a93e4d12660f96
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-11-20 02:02:14 -0500

    Updates

---
+++ precache-manifest.663bdff5418cf16115ac9a4ceace8892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e029b2096d460e921ce09d51e2f50d1",
+    "url": "/baby-names/static/media/bg.6e029b20.jpg"
+  },
+  {
+    "revision": "7093ead30be0a6c5ca6e",
+    "url": "/baby-names/static/js/runtime~main.7093ead3.js"
+  },
+  {
+    "revision": "1db4e1f242c05f2cb9a1",
+    "url": "/baby-names/static/js/main.1db4e1f2.chunk.js"
+  },
+  {
+    "revision": "3b31973540275cb76413",
+    "url": "/baby-names/static/js/1.3b319735.chunk.js"
+  },
+  {
+    "revision": "1db4e1f242c05f2cb9a1",
+    "url": "/baby-names/static/css/main.4336d0be.chunk.css"
+  },
+  {
+    "revision": "0739b6af4466a8cd78514866a5a61210",
+    "url": "/baby-names/index.html"
+  }
+];
